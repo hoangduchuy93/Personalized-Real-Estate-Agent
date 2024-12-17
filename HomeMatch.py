25db2c7e970b7668e925c507603d4cc0cbaeedb3
hoangduchuy93 <91864024+hoangduchuy93@users.noreply.github.com>
2024-12-17 21:01:12 +0700

    Delete HomeMatch.py

--- HomeMatch.py
+++
@@ -1,9 +0,0 @@
-#A starter file for the HomeMatch application if you want to build your solution in a Python program instead of a notebook. 
-
-import os
-
-os.environ["OPENAI_API_KEY"] = "voc-11866595711266773666260673c0de4ade9a6.68107284"
-os.environ["OPENAI_API_BASE"] = "https://openai.vocareum.com/v1"
-
-from langchain.llms import OpenAI
-
